8ec530c35b7304acc0c7d399ee3a041f80940ad0
lidingtonluke-create <lidingtonluke@gmail.com>
2026-05-29 17:47:31 -0700

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,1 +1,10 @@
-
+export default function handler(req, res) {
+  res.status(200).json({
+    name: "Black Hoodie",
+    category: "Hoodie",
+    color: "Black",
+    style: "Streetwear",
+    occasion: "Casual",
+    tags: ["hoodie", "casual", "streetwear"],
+  });
+}
